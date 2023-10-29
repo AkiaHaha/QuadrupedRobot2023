968c11c085d6b18ca12027b4e1c8b7b7c25cf648
AkiaHaha <daniel666cqu@163.com>
2023-10-29 14:03:09 +0800

    1

--- build/CMakeFiles/ellipseTrajectoryTest.dir/DependInfo.cmake
+++ build/CMakeFiles/ellipseTrajectoryTest.dir/DependInfo.cmake
@@ -1,22 +1,29 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/cplan.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/cplan.cpp.o"
+  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/main.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/main.cpp.o"
+  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/model.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/model.cpp.o"
+  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/robot.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/robot.cpp.o"
+  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/tplan.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/tplan.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNIX"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "F:/Gitrepo/singleLeg2023/src/cplan.cpp" "CMakeFiles/ellipseTrajectoryTest.dir/src/cplan.cpp.obj" "gcc" "CMakeFiles/ellipseTrajectoryTest.dir/src/cplan.cpp.obj.d"
-  "F:/Gitrepo/singleLeg2023/src/main.cpp" "CMakeFiles/ellipseTrajectoryTest.dir/src/main.cpp.obj" "gcc" "CMakeFiles/ellipseTrajectoryTest.dir/src/main.cpp.obj.d"
-  "F:/Gitrepo/singleLeg2023/src/model.cpp" "CMakeFiles/ellipseTrajectoryTest.dir/src/model.cpp.obj" "gcc" "CMakeFiles/ellipseTrajectoryTest.dir/src/model.cpp.obj.d"
-  "F:/Gitrepo/singleLeg2023/src/robot.cpp" "CMakeFiles/ellipseTrajectoryTest.dir/src/robot.cpp.obj" "gcc" "CMakeFiles/ellipseTrajectoryTest.dir/src/robot.cpp.obj.d"
-  "F:/Gitrepo/singleLeg2023/src/tplan.cpp" "CMakeFiles/ellipseTrajectoryTest.dir/src/tplan.cpp.obj" "gcc" "CMakeFiles/ellipseTrajectoryTest.dir/src/tplan.cpp.obj.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/aris/aris-2.3.9.230818/include"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/CMakeFiles/ellipseTrajectoryTest.dir/cmake_clean.cmake
+++ build/CMakeFiles/ellipseTrajectoryTest.dir/cmake_clean.cmake
@@ -1,18 +1,11 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/cplan.cpp.obj"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/cplan.cpp.obj.d"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/main.cpp.obj"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/main.cpp.obj.d"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/model.cpp.obj"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/model.cpp.obj.d"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/robot.cpp.obj"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/robot.cpp.obj.d"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/tplan.cpp.obj"
-  "CMakeFiles/ellipseTrajectoryTest.dir/src/tplan.cpp.obj.d"
-  "ellipseTrajectoryTest.exe"
-  "ellipseTrajectoryTest.exe.manifest"
+  "CMakeFiles/ellipseTrajectoryTest.dir/src/main.cpp.o"
+  "CMakeFiles/ellipseTrajectoryTest.dir/src/model.cpp.o"
+  "CMakeFiles/ellipseTrajectoryTest.dir/src/robot.cpp.o"
+  "CMakeFiles/ellipseTrajectoryTest.dir/src/tplan.cpp.o"
+  "CMakeFiles/ellipseTrajectoryTest.dir/src/cplan.cpp.o"
   "ellipseTrajectoryTest.pdb"
-  "libellipseTrajectoryTest.dll.a"
+  "ellipseTrajectoryTest"
 )
 
 # Per-language clean rules from dependency scanning.
