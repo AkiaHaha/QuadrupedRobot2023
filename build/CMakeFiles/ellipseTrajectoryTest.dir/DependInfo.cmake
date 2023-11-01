df565772cf26e57562c2e706bdff50a7a8ba83dc
AkiaHaha <daniel666cqu@163.com>
2023-11-01 13:15:06 +0800

    Single Leg Test

--- build/CMakeFiles/ellipseTrajectoryTest.dir/DependInfo.cmake
+++ build/CMakeFiles/ellipseTrajectoryTest.dir/DependInfo.cmake
@@ -4,11 +4,11 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/cplan.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/cplan.cpp.o"
-  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/main.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/main.cpp.o"
-  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/model.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/model.cpp.o"
-  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/robot.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/robot.cpp.o"
-  "/home/kaanh/Desktop/elliptical_trajectory_231021/src/tplan.cpp" "/home/kaanh/Desktop/elliptical_trajectory_231021/build/CMakeFiles/ellipseTrajectoryTest.dir/src/tplan.cpp.o"
+  "/home/kaanh/Desktop/singleLeg2023/src/cplan.cpp" "/home/kaanh/Desktop/singleLeg2023/build/CMakeFiles/ellipseTrajectoryTest.dir/src/cplan.cpp.o"
+  "/home/kaanh/Desktop/singleLeg2023/src/main.cpp" "/home/kaanh/Desktop/singleLeg2023/build/CMakeFiles/ellipseTrajectoryTest.dir/src/main.cpp.o"
+  "/home/kaanh/Desktop/singleLeg2023/src/model.cpp" "/home/kaanh/Desktop/singleLeg2023/build/CMakeFiles/ellipseTrajectoryTest.dir/src/model.cpp.o"
+  "/home/kaanh/Desktop/singleLeg2023/src/robot.cpp" "/home/kaanh/Desktop/singleLeg2023/build/CMakeFiles/ellipseTrajectoryTest.dir/src/robot.cpp.o"
+  "/home/kaanh/Desktop/singleLeg2023/src/tplan.cpp" "/home/kaanh/Desktop/singleLeg2023/build/CMakeFiles/ellipseTrajectoryTest.dir/src/tplan.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -19,7 +19,7 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/aris/aris-2.3.9.230818/include"
+  "/usr/aris/aris-2.3.6.230423/include"
   )
 
 # Targets to which this target links.
